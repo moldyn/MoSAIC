5046c330fb3256069491f9cce4f484bab85761d6
gd1022 <you@example.com>
2021-07-30 00:01:32 +0200

    added functions

--- src/cfs/clustering.py
+++ src/cfs/clustering.py
@@ -9,6 +9,7 @@ All rights reserved.
 import numpy as np
 import igraph as ig
 import leidenalg as la
+from sklearn.neighbors import NearestNeighbors
 
 
 class Clustering:
@@ -28,8 +29,11 @@ class Clustering:
         is constructed using the adjacency matrix.
 
     neighbors: int, default=None,
-        If NaN, the number of neighbors is chosen as the square root of the
-        number of features.
+        Only required for mode 'modularity'. If NaN, the number of neighbors
+        is chosen as the square root of the number of features.
+
+    resolution_parameter : float, default= 0.7
+        Only required for mode 'CPM'
 
     Attributes
     ----------
@@ -37,6 +41,7 @@ class Clustering:
         The result of the clustering process. A list containing of arrays,
         each containing all features for each cluster.
     """
+    _kwargs_leiden = {}
 
     def __init__(self, *, mode='CPM', weighted=True, neighbors=None):
         """Initializes Clustering class."""
@@ -49,19 +54,53 @@ class Clustering:
                 'CPM Leiden clustering works best on a fully weighted graph.'
             )
 
-
-    def _construct_graph(self, matrix, weighted, neighbors):
-        """Calculates the graph.
+    def fit(self, matrix, y=None):
+        """Clusters the correlation matrix by Leiden clustering on a graph.
 
         Parameters
         ----------
         matrix : ndarray of shape (n_features, n_features)
-            Contains the correlation data.
+            Matrix containing the correlation metric which is clustered.
 
-        weighted : bool,
-            Determines whether graph is weighted or unweighted
-
-        neighbors : int, default=sqrt(n_samples)
-            The number of k nearest neighbors for constructing the graph
+        y : Ignored
+            Not used, present for scikit API consistency by convention.
         """
+        if self._mode == 'CPM':
+            pass
 
+    def _construct_knn_graph(
+        self,
+        matrix,
+        knn=np.floor(np.sqrt(len(matrix))).astype(int),
+    ):
+        """Constructs the graph."""
+        neigh = NearestNeighbors(
+            n_neighbors=knn,
+            metric='precomputed',
+        )
+        if not self._weighted:
+            neigh.fit(1 - matrix)
+        else:
+            #TODO: continue
+
+    def _construct_full_weighted_graph(self, matrix):
+        """Constructs a full, weighted graph."""
+        graph = ig.Graph.Weighted_Adjacency(matrix, loops=False)
+        self._kwargs_leiden['weights'] = graph.es['weight']
+        return graph
+
+    def _setup_leiden_kwargs(self):
+        """Sets up the parameters for the Leiden clustering"""
+        if self._mode == 'CPM':
+            self._kwargs_leiden['partition_type'] = la.CPMVertexPartition
+            self._kwargs_leiden[
+                'resolution_parameter'
+            ] = self._resolution_parameter
+        else:
+            self._kwargs_leiden[
+                'partition_type'
+            ] = la.ModularityVertexPartition
+
+    def _clustering_leiden(self, graph):
+        """Performs the Leiden clustering on the graph."""
+        return la.find_partition(graph, **self._kwargs_leiden)
