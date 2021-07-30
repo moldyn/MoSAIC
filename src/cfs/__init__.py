c3f37cb98b285d5cbd2faf626c4f41c03d2a510d
braniii <daniel.nagel@protonmail.com>
2021-07-30 10:43:21 +0200

    Implemented further graph creation

--- src/cfs/__init__.py
+++ src/cfs/__init__.py
@@ -2,5 +2,6 @@
 """.. include:: ../../README.md"""
 
 from .similarity import Similarity
+from .clustering import Clustering
 
 __version__ = '0.1.0'
--- src/cfs/clustering.py
+++ src/cfs/clustering.py
@@ -37,21 +37,36 @@ class Clustering:
 
     Attributes
     ----------
-    clusters_ : list of arrays
-        The result of the clustering process. A list containing of arrays,
-        each containing all features for each cluster.
-    """
-    _kwargs_leiden = {}
+    clusters_ : list of ndarrays
+        The result of the clustering process. A list of arrays, each containing
+        all indices (features) for each cluster.
 
-    def __init__(self, *, mode='CPM', weighted=True, neighbors=None):
+    """
+    _available_modes = ('CPM', 'modularity')
+
+    def __init__(
+        self,
+        *,
+        mode='CPM',
+        weighted=True,
+        neighbors=None,
+        resolution_parameter=0.7,
+    ):
         """Initializes Clustering class."""
+        if mode not in self._available_modes:
+            raise NotImplementedError(
+                f'Mode {mode} is not implemented, use one of ['
+                f'{" ".join(self._available_modes)}].',
+            )
+
         self._mode = mode
         self._weighted = weighted
         self._neighbors = neighbors
+        self._resolution_parameter = resolution_parameter
 
         if self._mode == 'CPM' and not self._weighted:
-            raise ValueError(
-                'CPM Leiden clustering works best on a fully weighted graph.'
+            raise NotImplementedError(
+                'mode="CPM" does not support an unweighted=True.'
             )
 
     def fit(self, matrix, y=None):
@@ -60,47 +75,59 @@ class Clustering:
         Parameters
         ----------
         matrix : ndarray of shape (n_features, n_features)
-            Matrix containing the correlation metric which is clustered.
+            Matrix containing the correlation metric which is clustered. The
+            values should go from [0, 1] where 1 means completely correlated
+            and 0 no correlation.
 
         y : Ignored
             Not used, present for scikit API consistency by convention.
+
         """
         if self._mode == 'CPM':
-            pass
+            graph = self._construct_full_weighted_graph(matrix)
+        elif self._mode == 'modularity':
+            graph = self._construct_knn_graph(matrix)
+        else:
+            raise NotImplementedError(
+                f'Mode {self._mode} is not implemented',
+            )
+        self._clustering_leiden(graph)
 
-    def _construct_knn_graph(
-        self,
-        matrix,
-        knn=np.floor(np.sqrt(len(matrix))).astype(int),
-    ):
+    def _construct_knn_graph(self, matrix):
         """Constructs the graph."""
+        if self._neighbors is None:
+            self._neighbors = np.floor(np.sqrt(len(matrix))).astype(int)
         neigh = NearestNeighbors(
-            n_neighbors=knn,
+            n_neighbors=self._neighbors,
             metric='precomputed',
         )
-        if not self._weighted:
-            neigh.fit(1 - matrix)
-        else:
-            #TODO: continue
+        neigh.fit(1 - matrix)
+        dist_mat = neigh.kneighbors_graph(mode='distance').toarray()
+        return ig.Graph.Weighted_Adjacency(dist_mat, loops=False)
 
     def _construct_full_weighted_graph(self, matrix):
         """Constructs a full, weighted graph."""
-        graph = ig.Graph.Weighted_Adjacency(matrix, loops=False)
-        self._kwargs_leiden['weights'] = graph.es['weight']
-        return graph
+        return ig.Graph.Weighted_Adjacency(matrix, loops=False)
 
-    def _setup_leiden_kwargs(self):
+    def _setup_leiden_kwargs(self, graph):
         """Sets up the parameters for the Leiden clustering"""
+        kwargs_leiden = {}
         if self._mode == 'CPM':
-            self._kwargs_leiden['partition_type'] = la.CPMVertexPartition
-            self._kwargs_leiden[
+            kwargs_leiden['partition_type'] = la.CPMVertexPartition
+            kwargs_leiden[
                 'resolution_parameter'
             ] = self._resolution_parameter
         else:
-            self._kwargs_leiden[
+            kwargs_leiden[
                 'partition_type'
             ] = la.ModularityVertexPartition
+        if self._weighted:
+            kwargs_leiden['weights'] = graph.es['weight']
+
+        return kwargs_leiden
 
     def _clustering_leiden(self, graph):
         """Performs the Leiden clustering on the graph."""
-        return la.find_partition(graph, **self._kwargs_leiden)
+        self.clusters_ = la.find_partition(
+            graph, **self._setup_leiden_kwargs(graph),
+        )
