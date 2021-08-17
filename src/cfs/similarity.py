f15eabeedc84e20439b0162de01f55f5191d3e72
braniii <daniel.nagel@protonmail.com>
2021-08-17 11:37:41 +0200

    Add Gelfand and Yaglom normalization.

--- src/cfs/similarity.py
+++ src/cfs/similarity.py
@@ -69,6 +69,7 @@ class Similarity:  # noqa: WPS214
 
         - 'correlation' will use the absolute value of the Pearson correlation
         - 'NMI' will use the mutual information normalized by joined entropy
+        - 'GY' use Gel'fand and Yaglom normalization[1][GY1957]
         - 'JSD' will use the Jensen-Shannon divergence between the joint
           probability distribution and the product of the marginal probability
           distributions to calculate their dissimilarity
@@ -113,10 +114,14 @@ class Similarity:  # noqa: WPS214
     $$\rho_{X,Y} =
     \frac{\langle(X -\mu_X)(Y -\mu_Y)\rangle}{\sigma_X\sigma_Y}$$
     where for the online algorithm the Welford algorithm taken from Donald E.
-    Knuth were used [1][Knuth98].
+    Knuth were used [2][Knuth98].
+
+    [GY1957]: Gel'fand, I.M. and Yaglom, A.M. (1957). "Calculation of amount of
+    information about a random function contained in another such function".
+    American Mathematical Society Translations, series 2, 12, pp. 199–246.
 
     [Knuth98]: Welford algorithm, generalized to correlation. Taken from:
-    Donald E. Knuth (1998). The Art of Computer Programming, volume 2:
+    Donald E. Knuth (1998). "The Art of Computer Programming", volume 2:
     Seminumerical Algorithms, 3rd edn., p. 232. Boston: Addison-Wesley.
 
     The Jensen-Shannon divergence is defined as
@@ -129,7 +134,10 @@ class Similarity:  # noqa: WPS214
 
     _dtype: np.dtype = np.float128
     _default_normalize_method: str = 'arithmetic'
-    _available_metrics: Tuple[str] = ('correlation', 'NMI', 'JSD')
+    _available_metrics: Tuple[str, ...] = ('correlation', 'NMI', 'JSD', 'GY')
+    _available_norms: Tuple[str, ...] = (
+        'joint', 'geometric', 'arithmetic', 'min', 'max',
+    )
 
     @beartype
     def __init__(
@@ -141,7 +149,7 @@ class Similarity:  # noqa: WPS214
     ):
         """Initialize Similarity class."""
         if metric not in self._available_metrics:
-            metrics = ', '.join([f'"{m}"' for m in self._available_modes])
+            metrics = ', '.join([f'"{m}"' for m in self._available_metrics])
             raise NotImplementedError(
                 f'Metric {metric} is not implemented, use one of [{metrics}]',
             )
@@ -151,6 +159,12 @@ class Similarity:  # noqa: WPS214
         if self._metric == 'NMI':
             if normalize_method is None:
                 normalize_method = self._default_normalize_method
+            if normalize_method not in self._available_norms:
+                norms = ', '.join([f'"{n}"' for n in self._available_norms])
+                raise NotImplementedError(
+                    f'Normalization method {normalize_method} is not '
+                    f'implemented, use one of [{norms}]',
+                )
             self._normalize_method: str = normalize_method
         elif normalize_method is not None:
             raise NotImplementedError(
@@ -197,7 +211,7 @@ class Similarity:  # noqa: WPS214
             if self._metric == 'correlation':
                 corr = _correlation(X)
                 matrix_ = np.abs(corr)
-            elif self._metric in {'NMI', 'JSD'}:
+            elif self._metric in {'NMI', 'JSD', 'GY'}:
                 matrix_ = self._nonlinear_correlation(X)
             else:
                 raise NotImplementedError(
@@ -209,17 +223,21 @@ class Similarity:  # noqa: WPS214
     @beartype
     def _reset(self) -> None:
         """Reset internal data-dependent state of correlation."""
-        if hasattr(self, '_is_file'):  # noqa: WPS421
-            del self._is_file  # noqa: WPS420
+        if hasattr(self, '_filename'):  # noqa: WPS421
+            del self._filename  # noqa: WPS420
+        if hasattr(self, 'matrix_'):  # noqa: WPS421
             del self.matrix_  # noqa: WPS420
 
     @beartype
     def _nonlinear_correlation(self, X: np.ndarray) -> np.ndarray:
         """Return the nonlinear correlation."""
+        calc_nl_corr: Callable
         if self._metric == 'NMI':
-            calc_nl_corr: Callable = self._nmi
+            calc_nl_corr = self._nmi
+        elif self._metric == 'GY':
+            calc_nl_corr = self._gy
         else:
-            calc_nl_corr: Callable = self._jsd
+            calc_nl_corr = self._jsd
 
         nl_corr: np.ndarray = np.empty(  # noqa: WPS317
             (self._n_features, self._n_features), dtype=self._dtype,
@@ -234,6 +252,20 @@ class Similarity:  # noqa: WPS214
                 nl_corr[idx_j, idx_i] = nl_corr_ij
 
         return nl_corr
+
+    @beartype
+    def _gy(
+        self,
+        pij: np.ndarray,
+        pipj: np.ndarray,
+        pi: np.ndarray,
+        pj: np.ndarray,
+    ) -> float:
+        """Return the Jensen-Shannon based dissimilarity."""
+        mutual_info: float = _kullback(pij, pipj)
+        return -0.5 * np.sqrt(
+            1 - np.exp(-2 * mutual_info),
+        )
 
     @beartype
     def _nmi(
