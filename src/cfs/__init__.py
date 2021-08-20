d79163b6b01281f69cad4606e5e94c5fccface59
braniii <daniel.nagel@protonmail.com>
2021-08-20 11:22:47 +0200

    Sort init files import and add all definition for import *

--- src/cfs/__init__.py
+++ src/cfs/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """.. include:: ../../README.md"""
+__all__ = ['Clustering', 'Similarity']
 
-from .similarity import Similarity
 from .clustering import Clustering
+from .similarity import Similarity
+
 
 __version__ = '0.1.0'
