4dc73241af81582cbd7e3ee5151669c6fd56c3f5
braniii <daniel.nagel@protonmail.com>
2022-04-01 16:47:28 +0200

    Add first test of click cli. Check if help is shown when no args are specified

---
+++ tests/test___main__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""Tests for the cli script.
+
+MIT License
+Copyright (c) 2021-2022, Daniel Nagel
+All rights reserved.
+
+"""
+from click.testing import CliRunner
+
+from mosaic.__main__ import main
+
+
+def test_main():
+    runner = CliRunner()
+    result = runner.invoke(main)
+    assert result.exit_code == 0
+    assert '--help' in result.output
+
+
+def test_clustering():
+    runner = CliRunner()
+    result = runner.invoke(main, ['clustering'])
+    assert result.exit_code == 0
+    assert '--help' in result.output
+
+
+def test_clustering():
+    runner = CliRunner()
+    result = runner.invoke(main, ['similarity'])
+    assert result.exit_code == 0
+    assert '--help' in result.output
+
+
+def test_umap():
+    runner = CliRunner()
+    result = runner.invoke(main, ['umap'])
+    assert result.exit_code == 0
+    assert '--help' in result.output
