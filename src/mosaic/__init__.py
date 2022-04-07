78be8f3cf34f98e8e819a451130576c8fccf1af1
braniii <daniel.nagel@protonmail.com>
2022-04-07 16:26:59 +0200

    Move savetxt and save_clusters functionality to tools module

--- src/mosaic/__init__.py
+++ src/mosaic/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """.. include:: ../../README.md"""
-__all__ = ['Clustering', 'Similarity', 'UMAPSimilarity']
+__all__ = ['Clustering', 'load_clusters', 'Similarity', 'UMAPSimilarity']
 
 from .clustering import Clustering
 from .similarity import Similarity
+from .tools import load_clusters
 from .umap_similarity import UMAPSimilarity
 
 
---
+++ tests/test_tools.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""Tests for the tools module.
+
+MIT License
+Copyright (c) 2021-2022, Daniel Nagel
+All rights reserved.
+
+"""
+import numpy as np
+import pytest
+
+import mosaic
+
+
+def Xrand(N):
+    mat = np.random.uniform(size=N**2).reshape(N, N)
+    mat[np.diag_indices_from(mat)] = 1
+    mat = 0.5 * (mat + mat.T)
+    return mat
+
+
+@pytest.mark.parametrize('X', [Xrand(N=10), Xrand(N=30), Xrand(N=50)])
+def test_load_clusters(X):
+    clust = mosaic.Clustering(mode='CPM')
+    clust.fit(X)
+
+    # save clusters
+    filename = 'load_clusters_test'
+    clusters_string = np.array(
+        [
+            ' '.join([str(state) for state in cluster])
+            for cluster in clust.clusters_
+        ],
+        dtype=str,
+    )
+    np.savetxt(
+        filename,
+        clusters_string,
+        fmt='%s',
+    )
+
+    clusters = mosaic.load_clusters(filename)
+    assert len(clusters) == len(clust.clusters_)
+    for i in range(len(clusters)):
+        np.testing.assert_almost_equal(
+           clust.clusters_[i], clusters[i],
+        )
