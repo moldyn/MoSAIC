b5804b83ddbf2cbd3023dad24c6084a5bf2a4b31
braniii <daniel.nagel@protonmail.com>
2022-05-11 15:46:42 +0200

    Move correlation measure functions to own module to ease up testing.

---
+++ src/mosaic/_correlation_utils.py
@@ -0,0 +1,271 @@
+# -*- coding: utf-8 -*-
+"""Methods for calculating linear and non-linear correlation measures.
+
+MIT License
+Copyright (c) 2022, Daniel Nagel
+All rights reserved.
+
+"""
+import numpy as np
+from beartype import beartype
+from beartype.typing import Callable, Generator, Optional, Tuple, Union
+from scipy.spatial.distance import jensenshannon
+from sklearn import preprocessing
+from sklearn.feature_selection import mutual_info_regression
+
+from mosaic._typing import (  # noqa: WPS436
+    ArrayLikeFloat,
+    DTypeLike,
+    Float1DArray,
+    Float2DArray,
+    FloatMatrix,
+    MetricString,
+    NormString,
+    PositiveInt,
+)
+
+
+@beartype
+def _freedman_diaconis_rule(x: Float1DArray) -> int:
+    """Freedman Diaconis rule to estimate number of bins.
+
+    We replaced the prefactor 2 by 2.59 to asymptotically match Scott's normal
+    reference rule for a normal distribution. See
+    https://www.wikiwand.com/en/Freedman–Diaconis_rule
+
+    Freedman, David and Diaconis, Persi (1981):
+    "On the histogram as a density estimator: L2 theory"
+    Probability Theory and Related Fields. 57 (4), 453–476
+
+    """
+    scott_factor = 2.59
+    iqr = np.subtract(*np.percentile(x, [75, 25]))
+    binwidth = scott_factor * iqr / np.cbrt(len(x))
+    ptp = np.min([
+        np.subtract(*np.percentile(x, [85, 0])),
+        np.subtract(*np.percentile(x, [100, 15])),
+    ])
+    return int(
+        np.ceil(ptp / binwidth),
+    )
+
+
+@beartype
+def _entropy(p: ArrayLikeFloat) -> float:
+    """Calculate entropy of density p."""
+    return -1 * np.sum(p * np.ma.log(p))
+
+
+@beartype
+def _kullback(p: ArrayLikeFloat, q: ArrayLikeFloat) -> float:
+    """Calculate Kullback-Leibler divergence of density p, q."""
+    if len(p) != len(q):
+        raise ValueError(
+            f'Arrays p, q need to be of same length, but {len(p):.0f} vs '
+            f'{len(q):.0f}.',
+        )
+    return np.sum(
+        p * np.ma.log(np.ma.divide(p, q)),
+    )
+
+
+@beartype
+def _estimate_densities(
+    x: Float1DArray, y: Float1DArray, bins: Optional[PositiveInt] = None,
+) -> Tuple[Float2DArray, Float2DArray, Float1DArray, Float1DArray]:
+    """Calculate two dimensional probability densities."""
+    if bins is None:
+        bins = [
+            _freedman_diaconis_rule(x),
+            _freedman_diaconis_rule(y),
+        ]
+    hist, _, _ = np.histogram2d(x, y, bins=bins, density=True)
+    # transpose since numpy considers axis 0 as y and axis 1 as x
+    pxy = hist.T / np.sum(hist)
+    px = np.sum(pxy, axis=1)
+    py = np.sum(pxy, axis=0)
+    pxpy = px[:, np.newaxis] * py[np.newaxis, :]
+
+    return pxy, pxpy, px, py
+
+
+@beartype
+def _correlation(X: Float2DArray) -> FloatMatrix:
+    """Return the correlation of input.
+
+    Each feature (column) of X need to be mean-free with standard deviation 1.
+
+    """
+    return X.T / len(X) @ X
+
+
+@beartype
+def _knn_mutual_information(
+    X: Float2DArray,
+) -> FloatMatrix:
+    """Return the knn-estimated mutual information of input."""
+    n_features: PositiveInt
+    _, n_features = X.shape
+    mi_knn: FloatMatrix = np.empty((n_features, n_features))
+    for idx_feature, feature in enumerate(X.T):
+        mi_knn[idx_feature] = mutual_info_regression(
+            X, feature,
+        )
+    return mi_knn
+
+
+@beartype
+def _nonlinear_GY_knn(X: Float2DArray) -> FloatMatrix:
+    """Gel'fand-Yaglom correlation matrix based on k-nn estimator.
+
+    Return the nonlinear correlation matrix based on Gel'fand-Yaglom
+    based on a reliable knn-estimate of the mutual information.
+
+    """
+    nl_knn_corr = _knn_mutual_information(X)
+    return np.sqrt(
+        1 - np.exp(-2 * nl_knn_corr),
+    )
+
+
+@beartype
+def _gy(
+    pij: Float2DArray,
+    pipj: Float2DArray,
+    pi: Float1DArray,
+    pj: Float1DArray,
+) -> float:
+    """Return the dissimilarity based on Gel'fand-Yaglom."""
+    mutual_info: float = _kullback(pij, pipj)
+    return np.sqrt(
+        1 - np.exp(-2 * mutual_info),
+    )
+
+
+@beartype
+def _jsd(
+    pij: Float2DArray,
+    pipj: Float2DArray,
+    pi: Float1DArray,
+    pj: Float1DArray,
+) -> float:
+    """Return the Jensen-Shannon based dissimilarity."""
+    return jensenshannon(
+        pij.flatten(),
+        pipj.flatten(),
+        base=2,
+    )
+
+
+@beartype
+def _normalization(
+    pi: np.ndarray, pj: np.ndarray, pij: np.ndarray, method: NormString,
+) -> float:
+    """Calculate the normalization factor for the MI matrix."""
+    if method == 'joint':
+        return _entropy(pij)
+
+    func: Callable = {
+        'geometric': lambda arr: np.sqrt(np.prod(arr)),
+        'arithmetic': np.mean,
+        'min': np.min,
+        'max': np.max,
+    }[method]
+    return func([_entropy(pi), _entropy(pj)])
+
+
+@beartype
+def _nmi_gen(method: str) -> Callable:
+    @beartype
+    def _nmi(  # noqa: WPS430
+        pij: Float2DArray,
+        pipj: Float2DArray,
+        pi: Float1DArray,
+        pj: Float1DArray,
+    ) -> float:
+        """Return the Jensen-Shannon based dissimilarity."""
+        mutual_info: float = _kullback(pij, pipj)
+        normalization: float = _normalization(pi, pj, pij, method=method)
+        return mutual_info / normalization
+
+    return _nmi
+
+
+def _nonlinear_correlation_func(
+    metric: MetricString, normalize_method: NormString,
+) -> Callable:
+    """Return function to estimate non linear correlation."""
+    if metric == 'NMI':
+        return _nmi_gen(normalize_method)
+    elif metric == 'GY':
+        return _gy
+    # JSD
+    return _jsd
+
+
+@beartype
+def _data_gen(
+    *,
+    filename: str,
+    dtype: DTypeLike,
+    comments: Union[str, Tuple[str, ...]] = ('#', '@'),
+) -> Generator[Float1DArray, None, None]:
+    """Return all non comment lines as generator."""
+    with open(filename) as file_obj:
+        for line in file_obj:
+            if line.startswith(comments):
+                continue
+            yield np.array(line.split()).astype(dtype)
+
+
+@beartype
+def _get_n_features(
+    *,
+    filename: str,
+    dtype: DTypeLike,
+) -> int:
+    """Return the number of columns/features."""
+    return len(
+        next(
+            _data_gen(filename=filename, dtype=dtype),
+        ),
+    )
+
+
+@beartype
+def _welford_correlation(
+    *,
+    filename: str,
+    dtype: DTypeLike,
+) -> Tuple[FloatMatrix, int, int]:
+    """Calculate the correlation via online Welford algorithm.
+
+    Welford algorithm, generalized to correlation. Taken from:
+    Donald E. Knuth (1998). The Art of Computer Programming, volume 2:
+    Seminumerical Algorithms, 3rd edn., p. 232. Boston: Addison-Wesley.
+
+    """
+    n_samples: int = 0
+    n_features: int = _get_n_features(filename=filename, dtype=dtype)
+    mean: np.ndarray = np.zeros(n_features, dtype=dtype)
+    corr: np.ndarray = np.zeros(  # noqa: WPS317
+        (n_features, n_features), dtype=dtype,
+    )
+
+    for x in _data_gen(filename=filename, dtype=dtype):
+        n_samples += 1
+        dx: np.ndarray = x - mean
+        mean = mean + dx / n_samples
+        corr = corr + dx.reshape(-1, 1) * (
+            x - mean
+        ).reshape(1, -1)
+
+    if n_samples < 2:
+        return np.full_like(corr, np.nan), n_samples, n_features
+
+    std = np.sqrt(np.diag(corr) / (n_samples - 1))
+    corr = corr / (n_samples - 1) / (
+        std.reshape(-1, 1) * std.reshape(1, -1)
+    )
+
+    return corr, n_samples, n_features
--- tests/test_similarity.py
+++ tests/test_similarity.py
@@ -45,42 +45,6 @@ def X1_result(mode):
     }[mode]
 
 
-@pytest.mark.parametrize('p, result, error', [
-    ([1.0, np.e, 1.0], -np.e, None),
-    (np.ones(10), 0, None),
-    (np.arange(10).astype(np.float64), -79.05697962199447, None),
-    (np.arange(10).astype(np.float16), -79.0625, None),
-    (np.arange(10).astype(int), None, BeartypeException),
-    ([1, 2, 1], None, BeartypeException),
-    (['a', 'b'], None, BeartypeException),
-    (15, None, BeartypeException),
-])
-def test__entropy(p, result, error):
-    if not error:
-        np.testing.assert_almost_equal(
-            mosaic.similarity._entropy(p), result,
-        )
-    else:
-        with pytest.raises(error):
-            mosaic.similarity._entropy(p)
-
-
-@pytest.mark.parametrize('p, q, result, error', [
-    ([1., 1.], [1., 1.], 0, None),
-    ([1., 1.], [1 / np.e, 1.], 1, None),
-    ([1., 1., 1.], [1., 1.], None, ValueError),
-    ([1, 1], [1, 1], 0, BeartypeException),
-])
-def test__kullback(p, q, result, error):
-    if not error:
-        np.testing.assert_almost_equal(
-            mosaic.similarity._kullback(p, q), result,
-        )
-    else:
-        with pytest.raises(error):
-            mosaic.similarity._kullback(p, q)
-
-
 @pytest.mark.parametrize('X, error', [
     (np.random.uniform(size=(100, 20)), None),
     (np.random.uniform(size=(10, 5)), None),
@@ -99,47 +63,6 @@ def test__standard_scaler(X, error):
     else:
         with pytest.raises(error):
             mosaic.similarity._standard_scaler(X)
-
-
-@pytest.mark.parametrize('x, y, kwargs, error', [
-    (np.random.uniform(size=10000), np.random.uniform(size=10000), {}, None),
-    (
-        np.random.uniform(size=10000),
-        np.random.uniform(size=10000),
-        {'bins': 10},
-        None
-    ),
-])
-def test__estimate_densities(x, y, kwargs, error):
-    if not error:
-        densities = mosaic.similarity._estimate_densities(x, y, **kwargs)
-        _, _, px, py = densities
-
-        # check normalization
-        for p in densities:
-            np.testing.assert_almost_equal(np.sum(p), 1)
-    else:
-        with pytest.raises(error):
-            mosaic.similarity._estimate_densities(x, y, **kwargs)
-
-
-@pytest.mark.parametrize('X, result, error', [
-    (
-        np.array([[1.0, 1.0], [0.9, 0.8], [0.8, 0.6]]),
-        np.array([[49, 44], [44, 40]]) / 60,
-        None,
-    ),
-    (np.arange(10), None, BeartypeException),
-    (np.arange(10).astype(np.float64), None, BeartypeException),
-])
-def test__correlation(X, result, error):
-    if not error:
-        np.testing.assert_array_almost_equal(
-            mosaic.similarity._correlation(X), result,
-        )
-    else:
-        with pytest.raises(error):
-            mosaic.similarity._correlation(X)
 
 
 @pytest.mark.parametrize('metric, kwargs, X, result, error', [
