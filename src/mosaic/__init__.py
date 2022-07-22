93a0d9aa13d0b8e4d839d0d3f19ebc814d9dd3ab
braniii <daniel.nagel@protonmail.com>
2022-07-22 13:14:16 +0200

    bump version to v0.3.0

--- src/mosaic/__init__.py
+++ src/mosaic/__init__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 """.. include:: ../../README.md"""
-__all__ = ['Clustering', 'Similarity', 'UMAPSimilarity']
+__all__ = ['Clustering', 'GridSearchCV', 'Similarity', 'UMAPSimilarity']
 
 import mosaic.utils  # noqa: F401
 from .clustering import Clustering
+from .gridsearch import GridSearchCV
 from .similarity import Similarity
 from .umap_similarity import UMAPSimilarity
 
 
-__version__ = '0.2.2'
+__version__ = '0.3.0'
