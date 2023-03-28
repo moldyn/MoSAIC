4f3879ef3615bed147bf7b8bd33467d4a277f35e
braniii <daniel.nagel@protonmail.com>
2023-03-28 18:33:51 +0200

    Remove unused dep umap from setup.py

--- setup.py
+++ setup.py
@@ -106,7 +106,6 @@ setuptools.setup(
         'scikit-learn-extra',
         'igraph',
         'leidenalg>=0.8.0',
-        'umap-learn',
         'click>=7.0.0',
         'typing_extensions>=3.9.0;python_version<"3.9"',
         'prettypyplot',
