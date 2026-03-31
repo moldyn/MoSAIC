1a06201a3408febfb6c2943e7eb5d6e04323e3eb
gegabo <gega.diez@gmail.com>
2026-03-31 17:27:34 +0200

    fix(docs): mkdocstrings + Python 3.14 compat

--- docs/gen_ref_nav.py
+++ docs/gen_ref_nav.py
@@ -13,7 +13,7 @@ nav = mkdocs_gen_files.Nav()
 
 for path in sorted(Path('src').glob('**/*.py')):
     module_path = path.relative_to('src').with_suffix('')
-    doc_path = path.relative_to('src', 'mosaic').with_suffix('.md')
+    doc_path = path.relative_to(Path('src/mosaic')).with_suffix('.md')
     full_doc_path = Path('reference', doc_path)
 
     parts = list(module_path.parts)
